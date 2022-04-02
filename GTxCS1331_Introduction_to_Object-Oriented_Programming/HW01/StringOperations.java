4646561cde15b618c336c8450661e1030b99f5fc
Ken.Y <69562939+ken-100@users.noreply.github.com>
2022-04-02 23:41:07 +0900

    Create StringOperations.java

---
+++ GTxCS1331_Introduction_to_Object-Oriented_Programming/HW01/StringOperations.java
@@ -0,0 +1,12 @@
+public class StringOperations {
+    public static void main(String[] args) {
+        
+	    String g = new String("Ken");
+	    String h = g.replace("K", "A");
+	    String i = h.replace("n", "Z");
+	    
+	    System.out.println(g);
+	    System.out.println(h);
+	    System.out.println(i);	
+    }
+}
